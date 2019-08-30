d8371006e622fb9aaa6055c87e20cc1ac5696b0d
App Academy Student <aa@Apps-Mac-mini-5.local>
2019-08-30 12:59:05 -0400

    Complete AttrAccessorMethod

--- Active_Record_Part_1/lib/00_attr_accessor_object.rb
+++ Active_Record_Part_1/lib/00_attr_accessor_object.rb
@@ -1,19 +1,17 @@
 class AttrAccessorObject
   def self.my_attr_accessor(*names)
     names.each do |meth_name|
-      define_method(meth_name) do |change = meth_name|
-        if change == meth_name
-          instance_method_get(meth_name)
-        else 
-          instance_method_set(meth_name, change)
-        end 
+  
+      define_method("#{meth_name}=") do |change|
+        instance_variable_set("@#{meth_name}", change)
       end 
-    end 
 
-    # names.each do |meth_name| 
-    #   define_method(meth_name) do 
-    #     instance_method_get(meth_name)
-    #   end 
-    # end 
-  end
+      define_method("#{meth_name}") do 
+        instance_variable_get("@#{meth_name}")
+      end 
+
+    end
+
+  end 
+
 end
