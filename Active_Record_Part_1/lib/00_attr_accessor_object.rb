d0c9a6dc8da2c1ab4bd98fee6828ea207235ef8e
App Academy Student <aa@Apps-Mac-mini-5.local>
2019-08-30 12:15:49 -0400

    Accessor started

--- Active_Record_Part_1/lib/00_attr_accessor_object.rb
+++ Active_Record_Part_1/lib/00_attr_accessor_object.rb
@@ -2,9 +2,18 @@ class AttrAccessorObject
   def self.my_attr_accessor(*names)
     names.each do |meth_name|
       define_method(meth_name) do |change = meth_name|
-        instance_method_get(meth_name)
-        instance_method_set(meth_name, change)
+        if change == meth_name
+          instance_method_get(meth_name)
+        else 
+          instance_method_set(meth_name, change)
+        end 
       end 
     end 
+
+    # names.each do |meth_name| 
+    #   define_method(meth_name) do 
+    #     instance_method_get(meth_name)
+    #   end 
+    # end 
   end
 end
